3e502bcce8e7add1fe0a34fdd57d8ce3b7373226
syedusamamazhar <syedusamamazhar1991@gmail.com>
2017-05-06 16:45:28 +0500

    printing work done, only need to test on real printer

---
+++ Cheque/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cheque
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            string ReportName = "Report1";
+
+            LocalReport report = new LocalReport();
+
+            report.ReportEmbeddedResource = "Cheque." + ReportName + ".rdlc";
+
+            ReportParameter rp1 = new ReportParameter("Dated", Convert.ToDateTime(txtDated.Text.ToString()).ToString("dd/MM/yyy"));
+            ReportParameter rp2 = new ReportParameter("PayTo", txtPayto.Text.ToString());
+            ReportParameter rp3 = new ReportParameter("theSumOfRupees", txttheSumOfRupees.Text.ToString());
+            ReportParameter rp4 = new ReportParameter("Rs", txtRs.Text.ToString());
+
+            report.SetParameters(new ReportParameter[] { rp1, rp2, rp3, rp4 });
+
+            report.PrintToPrinter();
+        }
+
+        private void btnPdf_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Warning[] warnings;
+                string extension;
+                string[] streamids;
+                string mimeType;
+                string encoding;
+
+                ReportViewer _reportViewer = LoadReport();
+
+                if (_reportViewer != null)
+                {
+
+                    byte[] bytes = _reportViewer.LocalReport.Render(
+                       "PDF", null, out mimeType, out encoding,
+                        out extension,
+                       out streamids, out warnings);
+
+                    using (FileStream fs = new FileStream("output.pdf", FileMode.Create))
+                    {
+                        fs.Write(bytes, 0, bytes.Length);
+                    }
+
+                    MessageBox.Show("Cheque printing process initiated.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private ReportViewer LoadReport()
+        {
+            try
+            {
+                ReportViewer _reportViewer = new ReportViewer();
+                _reportViewer.Reset();
+
+                string ReportName = "Report1";
+
+                _reportViewer.LocalReport.ReportEmbeddedResource = "Cheque." + ReportName + ".rdlc";
+                //_reportViewer.LocalReport.ReportPath = ReportName + ".rdlc";
+
+                ReportParameter rp1 = new ReportParameter("Dated", Convert.ToDateTime(txtDated.Text.ToString()).ToString("dd/MM/yyy"));
+                ReportParameter rp2 = new ReportParameter("PayTo", txtPayto.Text.ToString());
+                ReportParameter rp3 = new ReportParameter("theSumOfRupees", txttheSumOfRupees.Text.ToString());
+                ReportParameter rp4 = new ReportParameter("Rs", txtRs.Text.ToString());
+
+                _reportViewer.LocalReport.SetParameters(new ReportParameter[] { rp1, rp2, rp3, rp4 });
+
+                _reportViewer.LocalReport.Refresh();
+
+                return _reportViewer;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Cheque/PrintReport.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Reporting.WinForms;
+using System.Reflection;
+using System.Drawing.Printing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace Cheque
+{
+    public static class PrintReport
+    {
+
+        private static int m_currentPageIndex;
+        private static IList<Stream> m_streams;
+
+        public static Stream CreateStream(string name,
+          string fileNameExtension, Encoding encoding,
+          string mimeType, bool willSeek)
+        {
+            Stream stream = new MemoryStream();
+            m_streams.Add(stream);
+            return stream;
+        }
+
+        public static void Export(LocalReport report, bool print = true)
+        {
+            string deviceInfo =
+              @"<DeviceInfo>
+                <OutputFormat>EMF</OutputFormat>
+                <PageWidth>8.5in</PageWidth>
+                <PageHeight>11in</PageHeight>
+                <MarginTop>0.1in</MarginTop>
+                <MarginLeft>0.1in</MarginLeft>
+                <MarginRight>0.1in</MarginRight>
+                <MarginBottom>0.1in</MarginBottom>
+            </DeviceInfo>";
+
+            Warning[] warnings;
+            m_streams = new List<Stream>();
+            report.Render("Image", deviceInfo, CreateStream,
+               out warnings);
+            foreach (Stream stream in m_streams)
+                stream.Position = 0;
+
+            if (print)
+            {
+                Print();
+            }
+        }
+
+
+        // Handler for PrintPageEvents
+        public static void PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            Metafile pageImage = new
+               Metafile(m_streams[m_currentPageIndex]);
+
+            // Adjust rectangular area with printer margins.
+            Rectangle adjustedRect = new Rectangle(
+                ev.PageBounds.Left - (int)ev.PageSettings.HardMarginX,
+                ev.PageBounds.Top - (int)ev.PageSettings.HardMarginY,
+                ev.PageBounds.Width,
+                ev.PageBounds.Height);
+
+            // Draw a white background for the report
+            ev.Graphics.FillRectangle(Brushes.White, adjustedRect);
+
+            // Draw the report content
+            ev.Graphics.DrawImage(pageImage, adjustedRect);
+
+            // Prepare for the next page. Make sure we haven't hit the end.
+            m_currentPageIndex++;
+            ev.HasMorePages = (m_currentPageIndex < m_streams.Count);
+        }
+
+        public static void Print()
+        {
+            if (m_streams == null || m_streams.Count == 0)
+                throw new Exception("Error: no stream to print.");
+            PrintDocument printDoc = new PrintDocument();
+            if (!printDoc.PrinterSettings.IsValid)
+            {
+                throw new Exception("Error: cannot find the default printer.");
+            }
+            else
+            {
+                printDoc.PrintPage += new PrintPageEventHandler(PrintPage);
+                m_currentPageIndex = 0;
+                printDoc.Print();
+            }
+        }
+
+        public static void PrintToPrinter(this LocalReport report)
+        {
+            Export(report);
+        }
+
+        public static void DisposePrint()
+        {
+            if (m_streams != null)
+            {
+                foreach (Stream stream in m_streams)
+                    stream.Close();
+                m_streams = null;
+            }
+        }
+    }
+}
